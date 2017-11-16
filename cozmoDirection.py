e691a37f67367882c6a170a3dd026ccf62fe0415
Jonathan White <white.jon@husky.neu.edu>
2017-11-15 22:05:16 -0800

    Fixed direction calculations

--- cozmoDirection.py
+++ cozmoDirection.py
@@ -3,61 +3,84 @@ import cozmo
 from cozmo.util import degrees, distance_inches, speed_mmps
 from enum import Enum
 
-class direction(Enum):
-    South = 0
-    North = 1
-    East = 2
-    West = 3
-    SouthEast = 4
-    NorthEast = 5
-    SouthWest = 6
-    NorthWest = 7
+DEGREES_PER_DIRECTION = 45
+
+class nav_direction:
+
+    def __init__(self, value, name):
+        self.value = value
+        self.name = name
+
+class nav(nav_direction):
+    North = nav_direction(0, "North")
+    NorthEast = nav_direction(1, "NorthEast")
+    East = nav_direction(2, "East")
+    SouthEast = nav_direction(3, "SouthEast")
+    South = nav_direction(4, "South")
+    SouthWest = nav_direction(5, "SouthWest")
+    West = nav_direction(6, "West")
+    NorthWest = nav_direction(7, "NorthWest")
+
+# We assume Cozmo is initialized to face North
+#class direction(Enum):
+#    North = 0
+#    NorthEast = 1
+#    East = 2
+#    SouthEast = 3
+#    South = 4
+#    SouthWest = 5
+#    West = 6
+#    NorthWest = 7
 
 class cozmo_motion:
 
     def __init__(self):
-        self.DEGREE = 0
+        self.orientation = nav.North
 
-    def pointDirection(self, newpoint, oldpoint):
-        dx = newpoint.x - oldpoint.x
-        dy = newpoint.y - oldpoint.y
-        if (dx == 0 and dy == 1):
-            value = 0
-        elif (dx == 0 and dy == 1):
-            value = 1
-        elif (dx == 1 and dy == 0):
-            value =2
-        elif (dx == -1 and dy == 0):
-            value =3
-        elif (dx == 1 and dy == 1):
-            value = 4
+    def pointDirection(self, next_point, current_point):
+
+        # Get deltas
+        dx = next_point.x - current_point.x
+        dy = next_point.y - current_point.y
+
+        # Determine direction of movement
+        if (dx == 0 and dy == -1):
+            new_orientation = nav.North
         elif (dx == 1 and dy == -1):
-            value = 5
+            new_orientation = nav.NorthEast
+        elif (dx == 1 and dy == 0):
+            new_orientation = nav.East
+        elif (dx == 1 and dy == 1):
+            new_orientation = nav.SouthEast
+        elif (dx == 0 and dy == 1):
+            new_orientation = nav.South
         elif (dx == -1 and dy == 1):
-            value = 6
+            new_orientation = nav.SouthWest
+        elif (dx == -1 and dy == 0):
+            new_orientation = nav.West
         else:
-            value = 7
-        self.cozmoDirection(value)
+            new_orientation = nav.NorthWest
 
-    def cozmoDirection(self, direct):
-        if direct == 0:
-            self.DEGREE = self.DEGREE + 0
-        elif direct == 1:
-            self.DEGREE = self.DEGREE + 180
-        elif direct == 2:
-            self.DEGREE = self.DEGREE - 90
-        elif direct == 3:
-            self.DEGREE = self.DEGREE + 90
-        elif direct == 4:
-            self.DEGREE = self.DEGREE - 45
-        elif direct == 5:
-            self.DEGREE = self.DEGREE - 135
-        elif direct == 6:
-            self.DEGREE = self.DEGREE + 45
-        else:
-            self.DEGREE = self.DEGREE + 135
-        print ("direction / degree", direct, self.DEGREE)
+        # Find rotational angle
+        print("    Direction: ", new_orientation.name)
+        self.get_rotation_angle(new_orientation)
+
+    def get_rotation_angle(self, target_orientation):
+        
+        # Calculate degrees to rotate
+        degrees = (target_orientation.value - self.orientation.value ) * DEGREES_PER_DIRECTION
+
+        # Always rotate the shortest amount necessary
+        if degrees > 180:
+            degrees -= 360
+        if degrees < -180:
+            degrees += 360
+
+        print ("    Rotating {} degrees".format(degrees))
+
+        # Set new orientation
         #self.cozmo_program(cozmo.robot.Robot, self.DEGREE)
+        self.orientation = target_orientation
 
     def cozmo_program(robot: cozmo.robot.Robot, degree):
         if degree != 0 :
